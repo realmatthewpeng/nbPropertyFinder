aabbfad7549244bf0c6db701288355648ee9d4c3
Matthew Peng <mapeng@ucsd.edu>
2024-02-15 00:44:32 -0800

    some refactorings

--- tool.py
+++ tool.py
@@ -4,85 +4,93 @@ import os
 
 class Visitor(ast.NodeVisitor):
 
-    savedLines: set()
+    savedLines: set
     knownFuncNames = ["head", "describe", "dropna", "isnull", "copy", 
                         "astype", "drop", "shape", "index", "mean", "std",
                         "quantile", "size", "rename", "count", "sort_values",
-                        "loc", "iloc", "max", "tail", "columns", "fillna"]
+                        "loc", "iloc", "max", "tail", "columns", "fillna",
+                        "apply"]
 
     def __init__(self):
         super()
         self.savedLines = set()
 
     def visit(self, node: ast.AST):
-        # print(type(node))
-        #if hasattr(node, "attr"):
-        #    print(node.attr, end="\n\n")
-
         if type(node) == ast.Call:
-            if isinstance(node.func, ast.Attribute):
-                if node.func.attr in self.knownFuncNames:
-                    self.savedLines.add((node.lineno, node.end_lineno))
-            else:
-                args = node.args
-                for arg in args:
-                    if isinstance(arg, ast.Attribute):
-                        if arg.attr in self.knownFuncNames:
-                            self.savedLines.add((node.lineno, node.end_lineno))
+            self.visit_Call(node)
         elif type(node) == ast.Subscript:
-            if isinstance(node.slice, ast.Compare):
-                self.savedLines.add((node.lineno, node.end_lineno))
+            self.visit_Subscript(node)
         elif type(node) == ast.Expr:
-            if isinstance(node.value, ast.Name):
-                self.savedLines.add((node.lineno, node.end_lineno))
-            elif isinstance(node.value, ast.Subscript) and isinstance(node.value.slice, ast.List):
-                self.savedLines.add((node.lineno, node.end_lineno))
-            elif isinstance(node.value, ast.Attribute):
-                if node.value.attr in self.knownFuncNames:
-                    self.savedLines.add((node.lineno, node.end_lineno))
+            self.visit_Expr(node)
         elif type(node) == ast.Assign:
-            if isinstance(node.value, ast.Subscript):
-                self.savedLines.add((node.lineno, node.end_lineno))
-            targets = node.targets
-            for target in targets:
-                if isinstance(target, ast.Subscript):
-                    if isinstance(target.slice, ast.Constant):
-                        if (isinstance(target.slice.value, str)):
-                            self.savedLines.add((node.lineno, node.end_lineno))
-                    elif isinstance(target.value, ast.Attribute):
-                        if target.value.attr in ["loc", "iloc"]:
-                            self.savedLines.add((node.lineno, node.end_lineno))
-        
+            self.visit_Assign(node)
+
         self.generic_visit(node)
+
+    def visit_Assign(self, node: ast.Assign):
+        if isinstance(node.value, ast.Subscript):
+            self.saveNode(node)
+        targets = node.targets
+        for target in targets:
+            if isinstance(target, ast.Subscript):
+                if isinstance(target.slice, ast.Constant):
+                    if (isinstance(target.slice.value, str)):
+                        self.saveNode(node)
+                elif isinstance(target.value, ast.Attribute):
+                    if target.value.attr in ["loc", "iloc"]:
+                        self.saveNode(node)
+
+    def visit_Expr(self, node: ast.Expr):
+        if isinstance(node.value, ast.Name):
+            self.saveNode(node)
+        elif isinstance(node.value, ast.Subscript) and isinstance(node.value.slice, ast.List):
+            self.saveNode(node)
+        elif isinstance(node.value, ast.Attribute):
+            if node.value.attr in self.knownFuncNames:
+                self.saveNode(node)
+
+    def visit_Call(self, node: ast.Call):
+        if isinstance(node.func, ast.Attribute):
+            if node.func.attr in self.knownFuncNames:
+                self.saveNode(node)
+        else:
+            args = node.args
+            for arg in args:
+                if isinstance(arg, ast.Attribute):
+                    if arg.attr in self.knownFuncNames:
+                        self.saveNode(node)
+    
+    def visit_Subscript(self, node: ast.Subscript):
+        if isinstance(node.slice, ast.Compare):
+            self.saveNode(node)
+
+    def saveNode(self, node: ast.AST):
+        self.savedLines.add((node.lineno, node.end_lineno, node.col_offset, node.end_col_offset))
 
     def getSaved(self):
         return self.savedLines
 
-TEST_SINGLE_NB = False
+TEST_SINGLE_NB = True
+NB_TEST_DIR = "notebooks/"
+NB_EXPECTED_DIR = "expected/"
+TEST_NB = "test.ipynb"
 
 def main():
-
-    nb_testdir = "notebooks/"
-    nb_expectdir = "expected/"
-    test_nb = "test.ipynb"
-
     if (TEST_SINGLE_NB):
-        nb = nbformat.read(test_nb, as_version=nbformat.NO_CONVERT)
-        processNotebook(nb, test_nb, nb_expectdir)
+        nb = nbformat.read(TEST_NB, as_version=nbformat.NO_CONVERT)
+        processNotebook(nb, TEST_NB)
     else:
-        for subdir, dirs, files in os.walk(nb_testdir):
+        for subdir, _, files in os.walk(NB_TEST_DIR):
             for file in files:
                 notebook = os.path.join(subdir, file)
                 nb = nbformat.read(notebook, as_version=nbformat.NO_CONVERT)
-                processNotebook(nb, file, nb_expectdir)
+                processNotebook(nb, file)
 
-def processNotebook(nb, file, nb_expectdir):
-    outfile = open("output/"+file.removesuffix(".ipynb")+".txt", 'w')
-
+def processNotebook(nb, file: str):
     found_lines = []
     for cell in nb["cells"]:
         if cell["cell_type"] == "code":
-            source = cell['source']
+            source: str = cell['source']
             try:
                 tree = ast.parse(source)
                 if TEST_SINGLE_NB: print(ast.dump(tree, indent=3), end="\n\n")
@@ -96,20 +104,24 @@ def processNotebook(nb, file, nb_expectdir):
             source_split = [line.strip() for line in source.split("\n")]
             if len(lines) != 0:
                 if TEST_SINGLE_NB: print(lines)
-                for (start,end) in lines:
+                for (start,end, _, _) in lines:
                     clean = "".join(source_split[start-1:end])
                     if TEST_SINGLE_NB: print(clean)
                     found_lines.append(clean)
-                # print("")
+    
+    processResults(found_lines, file)
 
-    # for line in all_lines:
-    #     print(line)
+
+def processResults(found_lines: list[str], file: str):
+
+    outfile = open("output/"+file.removesuffix(".ipynb")+".txt", 'w')
+
     print("Total number of lines found: " + str(len(found_lines)) + "\n", file=outfile)
 
     for line in found_lines:
         print(line, file=outfile)
 
-    expected = os.path.join(nb_expectdir, file.removesuffix(".ipynb")+".txt")
+    expected = os.path.join(NB_EXPECTED_DIR, file.removesuffix(".ipynb")+".txt")
     expected_lines = []
     with open(expected) as f:
         expected_lines = f.read().splitlines()
@@ -129,6 +141,7 @@ def processNotebook(nb, file, nb_expectdir):
 
     for line in expected_lines:
         print(line, file=outfile)
+
 
 if __name__ == "__main__":
     main()
