a8eb55cd4c07425067096ce9034f33d5a31d2602
Matthew Peng <mapeng@ucsd.edu>
2024-04-21 17:47:09 -0700

    added analysis of last lines in cells

---
+++ final_line_tool.py
@@ -0,0 +1,113 @@
+import nbformat
+import ast
+import os
+from operator import itemgetter
+import pandas as pd
+
+TEST_SINGLE_NB = False
+NB_TEST_DIR = "notebooks/"
+TEST_NB = "test.ipynb"
+
+sample_funcs = ["head", "tail", "loc", "iloc"]
+reduce_funs = ["describe", "mean", "std",
+                "size", "count", "max", 
+                "unique", "value_counts"]
+
+def main():
+    
+    all_last_lines = []
+    if (TEST_SINGLE_NB):
+        nb = nbformat.read(TEST_NB, as_version=4)
+        res = processNotebook(nb, TEST_NB)
+        if (res is not None):
+            all_last_lines.extend(res)
+    else:
+        for subdir, _, files in os.walk(NB_TEST_DIR):
+            for file in files:
+                notebook = os.path.join(subdir, file)
+                nb = nbformat.read(notebook, as_version=4)
+                res = processNotebook(nb, file)
+                if (res is not None):
+                    all_last_lines.extend(res)
+    
+    with open('all_last_lines_data.txt', 'w') as f:
+        for line in all_last_lines:
+            f.write(f"{line}\n")
+
+    df = pd.DataFrame(all_last_lines)
+    df.to_csv('all_last_lines.csv')
+
+def processNotebook(nb, file: str):
+
+    imported_pd = False
+    source_dict = {}
+    found_line_dict = {}
+
+    cell_count = 0
+    for cell in nb["cells"]:
+        if cell["cell_type"] == "code":
+            source: str = cell['source']
+            if (not imported_pd):
+                if "pandas" in source:
+                    imported_pd = True
+            cell_id = cell_count
+            try:
+                tree = ast.parse(source)
+                if (len(tree.body) > 0):
+                    node = tree.body[-1]
+                    category = "PRINT"
+                    if type(node) == ast.Expr:
+                        if type(node.value) == ast.Call:
+                            if type(node.value.func) == ast.Attribute:
+                                if node.value.func.attr in sample_funcs:
+                                    category = "SAMPLE"
+                                if node.value.func.attr in reduce_funs:
+                                    category = "REDUCTION"
+                            if type(node.value.func) == ast.Name:
+                                if (node.value.func.id == "print" or node.value.func.id == "display"):
+                                    if type(node.value.args[0]) == ast.Call:
+                                        if type(node.value.args[0].func) == ast.Attribute:
+                                            if (node.value.args[0].func.attr in sample_funcs):
+                                                category = "SAMPLE"
+                                            if (node.value.args[0].func.attr in reduce_funs):
+                                                category = "REDUCTION"
+                        if type(node.value) == ast.Subscript:
+                            category = "SAMPLE"
+                        if type(node.value) == ast.Name:
+                            category = "SAMPLE"
+                    else:
+                        category = "NO_PRINT"
+                    found_line_dict[cell_id] = (node.lineno, node.end_lineno, node.col_offset, node.end_col_offset, category)
+                if TEST_SINGLE_NB:
+                    print(category)
+                    print(ast.dump(tree, indent=3), end="\n\n")
+                    pass
+            except Exception as e:
+                # print(f"Error {e} in file {file}")
+                continue
+
+            source_dict[cell_id] = source
+
+        cell_count += 1
+
+    if (not imported_pd):
+        return
+    
+    all_cleaned = []
+    for id, source in source_dict.items():
+        source_split = [line.strip() for line in source.split("\n")]
+        if id in found_line_dict:
+            (start_r, end_r, start_c, end_c, category) = found_line_dict[id]
+            cut = source_split[start_r-1:end_r]
+            if len(cut) > 1:
+                cut[0] = cut[0][start_c:]
+                cut[-1] = cut[-1][:end_c]
+            else:
+                cut[0] = cut[0][start_c:end_c]
+            clean = "".join(cut)
+            all_cleaned.append({"category":category, "source":clean})
+
+    return all_cleaned
+
+if __name__ == "__main__":
+    main()
